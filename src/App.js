f156f5d4a9a3451d675b5ef84cd46cbcd394fbf3
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-09-13 13:01:49 +0530

    Footer setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header_footer/Header";
+import Footer from "./components/header_footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header_footer/Footer.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer_content">
+        <div className="footer_section about">
+          <h1 className="logo-text">
+            <span>Nature: </span>The tree hub
+          </h1>
+          <p>
+            Treehub is website where the customers can buy the the plants and
+            seeds for their home. It provides the customers with the facility to
+            order them from the nearby nurseries
+          </p>
+          <div className="contact">
+            <span>
+              <i class="fa fa-phone"></i> &nbsp; 123-456-789
+            </span>
+            <span>
+              <i className="fa fa-envelope"></i> &nbsp; info@treehub.com
+            </span>
+          </div>
+          <div className="socials">
+            <a href="#" style={{ color: "white" }}>
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a href="#" style={{ color: "white" }}>
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#" style={{ color: "white" }}>
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a href="#" style={{ color: "white" }}>
+              <i className="fa fa-youtube"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="footer_section links">
+          <h2>Quick Links</h2>
+          <ul>
+            <a href="#" className="link">
+              <li>Team</li>
+            </a>
+            <a href="#" className="link">
+              <li>FAQ and Guidlines</li>
+            </a>
+            <a href="#" className="link">
+              <li>Help</li>
+            </a>
+            <a href="#" className="link">
+              <li>Notifications</li>
+            </a>
+            <a href="#" className="link">
+              <li>Gallery</li>
+            </a>
+            <a href="#" className="link">
+              <li>Terms and Conditions</li>
+            </a>
+          </ul>
+        </div>
+        <div className="footer_section contact_form">
+          <h2>Contact Us</h2>
+          <form action="#">
+            <input
+              type="email"
+              name="email"
+              className="text-input contact-input"
+              placeholder="Your email address..."
+            />
+            <textarea
+              name="message"
+              className="text-input contact-input"
+              placeholder="Your message..."
+            ></textarea>
+            <button type="submit" className="btn btn-big">
+              <i className="fa fa-envelope"></i> Send
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="footer_bottom">
+        &copy; treehub.com | Experience the nature
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
