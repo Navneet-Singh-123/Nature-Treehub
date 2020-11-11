78667c5e9a4d22dbebb6f768c94691194529fb88
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-11-11 10:37:33 +0530

    Few updates

--- src/components/header_footer/GeneralHeader.js
+++ src/components/header_footer/GeneralHeader.js
@@ -4,11 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
 import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import SideDrawer from "./SideDrawer";
-import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
